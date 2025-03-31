0f2ed7c1fa0074f6c83cad28b20dffd3ea874a13
lcw3176 <lcw3176@naver.com>
2025-03-31 22:47:28 +0900

    add bucket cleaner

--- src/main/java/com/api/hitshot/HitShotApplication.java
+++ src/main/java/com/api/hitshot/HitShotApplication.java
@@ -2,8 +2,10 @@ package com.api.hitshot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HitShotApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/api/hitshot/domain/buckets/BucketCounter.java
+++ src/main/java/com/api/hitshot/domain/buckets/BucketCounter.java
@@ -2,7 +2,9 @@ package com.api.hitshot.domain.buckets;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -10,12 +12,19 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@Slf4j
 public class BucketCounter {
 
     private static final Map<String, Bucket> BUCKET_CACHE = new ConcurrentHashMap<>();
 
     @Value("${bucket.token-count}")
     private int TOKENS;
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void clearBucket() {
+        log.info("run bucket cleaner");
+        BUCKET_CACHE.clear();
+    }
 
     public Bucket getBucketForDomain(String domain) {
         return BUCKET_CACHE.computeIfAbsent(domain, key -> createNewBucket());
