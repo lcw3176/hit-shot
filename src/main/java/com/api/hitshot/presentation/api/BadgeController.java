aa9866c6029727ca72071f0910e14ee492a09ad8
lcw3176 <lcw3176@naver.com>
2025-04-03 00:05:25 +0900

    change controller logic

--- src/main/java/com/api/hitshot/presentation/api/BadgeController.java
+++ src/main/java/com/api/hitshot/presentation/api/BadgeController.java
@@ -1,6 +1,9 @@
 package com.api.hitshot.presentation.api;
 
-import com.api.hitshot.application.SiteUseCase;
+import com.api.hitshot.application.LogoMaker;
+import com.api.hitshot.application.UrlParser;
+import com.api.hitshot.application.VisitCounter;
+import com.api.hitshot.application.model.SiteVisitor;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.http.HttpHeaders;
@@ -17,17 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class BadgeController {
 
-    private final SiteUseCase siteUseCase;
+    private final VisitCounter visitCounter;
+    private final UrlParser urlParser;
+    private final LogoMaker logoMaker;
 
 
     @GetMapping("/svg")
     public ResponseEntity<String> getBadgeWithIncrement(@URL @RequestParam(value = "url") String url,
                                                         @RequestParam(defaultValue = "#4c1") String color) {
 
-        String filteredUrl = siteUseCase.urlValidation(url);
+        String filteredUrl = urlParser.makeValidFormat(url);
 
-        long visitor = siteUseCase.increaseAndGetCount(filteredUrl);
-        String logo = siteUseCase.makeSvgLogo(visitor, color);
+        SiteVisitor visitor = visitCounter.increaseAndGetCount(filteredUrl);
+        String logo = logoMaker.makeTodaySvgLogo(visitor.getToday(), visitor.getTotal(), color);
 
         return ResponseEntity.ok()
                 .headers(httpHeaders -> httpHeaders.add(HttpHeaders.CONTENT_TYPE, "image/svg+xml; charset=utf-8"))
@@ -38,9 +43,9 @@ public class BadgeController {
     @GetMapping("/hidden")
     public ResponseEntity<Void> increaseCountWithoutLogo(@URL @RequestParam(value = "url") String url) {
 
-        String filteredUrl = siteUseCase.urlValidation(url);
+        String filteredUrl = urlParser.makeValidFormat(url);
 
-        siteUseCase.increaseAndGetCount(filteredUrl);
+        visitCounter.increaseAndGetCount(filteredUrl);
 
         return ResponseEntity.ok().build();
     }
@@ -50,10 +55,10 @@ public class BadgeController {
     public ResponseEntity<String> getBadgeWithoutIncrement(@URL @RequestParam(value = "url") String url,
                                                            @RequestParam(defaultValue = "#4c1") String color) {
 
-        String filteredUrl = siteUseCase.urlValidation(url);
+        String filteredUrl = urlParser.makeValidFormat(url);
 
-        long visitor = siteUseCase.onlyGetVisitorsCount(filteredUrl);
-        String logo = siteUseCase.makeSvgLogo(visitor, color);
+        long totalVisitor = visitCounter.onlyGetTotalVisitorsCount(filteredUrl);
+        String logo = logoMaker.makeOnlyTotalSvgLogo(totalVisitor, color);
 
         return ResponseEntity.ok()
                 .headers(httpHeaders -> httpHeaders.add(HttpHeaders.CONTENT_TYPE, "image/svg+xml; charset=utf-8"))
